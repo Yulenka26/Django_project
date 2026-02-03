6c8fde345e7ccce62c1ae49a164104d6dcc9156d
Yuliya <parfenovais@lad24.ru>
2026-02-03 20:47:27 +0300

    Add fixes from pre-commit

--- src/project/blog_app/views.py
+++ src/project/blog_app/views.py
@@ -1,14 +1,22 @@
-from project.blog_app.forms import PostForm
+from project.blog_app.forms import PostForm, SearchForm, CategoryForm
 from project.blog_app.models import Post, Category
 from django.shortcuts import get_object_or_404, render, redirect
 from slugify import slugify
 
 
 def index(request):
-    posts = Post.objects.filter(published=True).order_by("-created_at")[:5]
+    posts = Post.objects.filter(published=True).order_by("-created_at")
+
+    form = SearchForm(request.GET)
+
+    if form.is_valid():
+        query = form.cleaned_data["query"].strip()
+        if query:
+            posts = posts.filter(title__icontains=query)
 
     context = {
-        "posts": posts
+        "posts": posts,
+        "form": form
     }
     return render(request, 'blog_app/index.html', context=context)
 
@@ -60,6 +68,46 @@ def post_create(request):
         form = PostForm()
 
     context = {
+        "form": form,
+        "title": "Добавление статьи",
+        "button_text": "Добавить"
+    }
+    return render(request, "blog_app/create_post.html", context=context)
+
+def category_create(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            new_category = form.save(commit=False)
+            new_category.save()
+
+            return redirect("blog:categories_list")
+    else:
+        form = CategoryForm()
+
+    context = {
         "form": form
     }
+    return render(request, "blog_app/create_post.html", context=context)
+
+def edit_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            edited_post = form.save(commit=False)
+            edited_post.slug = slugify(edited_post.title)
+            edited_post.save()
+            return redirect("blog:post_detail", edited_post.slug)
+    else:
+        form = PostForm(instance=post)
+
+    context = {
+        "form": form,
+        "title": "Редактирование статьи",
+        "button_text": "Сохранить"
+    }
+
     return render(request, "blog_app/create_post.html", context=context)
