55f2dfc2713a63452ced0ac523d8859178f1108c
Yuliya <parfenovais@lad24.ru>
2026-02-10 22:03:50 +0300

    dd fixes from pre-commit

--- src/project/blog_app/urls.py
+++ src/project/blog_app/urls.py
@@ -6,10 +6,10 @@ app_name = "blog"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path("post_list/", views.PostListView.as_view(), name='post_list'),
-    path("post/<slug:slug>/edit/", views.edit_post, name='edit_post'),
+    path("post/<slug:slug>/edit/", views.PostUpdateView.as_view(), name='edit_post'),
     path("post/<slug:slug>/", views.PostDetailView.as_view(), name='post_detail'),
-    path("categories/", views.categories_list, name='categories_list'),
-    path("categories/<int:category_id>/", views.category_detail, name='category_detail'),
-    path("create_post/", views.post_create, name='create_post'),
+    path("categories/", views.CategoriesListView.as_view(), name='categories_list'),
+    path("categories/<int:category_id>/", views.CategoryDetailView.as_view(), name='category_detail'),
+    path("create_post/", views.PostCreateView.as_view(), name='create_post'),
     path("category/create/", views.CategoryCreateView.as_view(), name='create_category'),
 ]
