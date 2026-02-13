44232d2cb450197d11dff1d13df2480f65c8d429
Yuliya <parfenovais@lad24.ru>
2026-02-13 22:33:38 +0300

    Доделала страницу с данными пользователя

--- src/project/users_app/urls.py
+++ src/project/users_app/urls.py
@@ -5,4 +5,5 @@ from project.users_app import views
 app_name = "users"
 urlpatterns = [
     path("", views.ProfileUpdateView.as_view(), name="user_page"),
+    path("profile/", views.ProfileDetailView.as_view(), name="user_profile"),
 ]
--- src/project/users_app/views.py
+++ src/project/users_app/views.py
@@ -1,16 +1,26 @@
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, DetailView
 
+from project.users_app.forms import UsersForm
 from project.users_app.models import Profile
 
 
 class ProfileUpdateView(UpdateView):
     model = Profile
-    fields = ["bio", "social_link"]
+    form_class = UsersForm
     template_name = "users_app/user_page.html"
-    success_url = reverse_lazy("users:user_page")
+    success_url = reverse_lazy("users:user_profile")
 
     # Проверяем, есть ли профиль. если нет - создаем
     def get_object(self, queryset=None):
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "users_app/user_profile.html"
+    context_object_name = "profile"
+
+    # Показываем профиль пользователя
+    def get_object(self):
+        return self.request.user.profile
