decab2b00eeae11d41bd5d73e0fd1da9bfb5eb70
Yuliya <parfenovais@lad24.ru>
2026-02-20 22:38:25 +0300

    Add fixes from pre-commit

--- src/project/blog_app/context_processors.py
+++ src/project/blog_app/context_processors.py
@@ -1,7 +1,15 @@
 from project.blog_app.models import Category
+from project.blog_app.models import Post
+from project.users_app.models import Profile
 
 
 def categories_processor(request):
     return {
         'nav_categories': Category.objects.all()
+    }
+
+def blog_stats(request):
+    return {
+        "nav_post_count": Post.objects.filter(published=True).count(),
+        "nav_user_count": Profile.objects.count()
     }
