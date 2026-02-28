5c7adef52c3b048ae1bdccbc566d62aee288547b
Yuliya <parfenovais@lad24.ru>
2026-02-28 22:24:31 +0300

    Разрешения для пользователей

--- src/project/drf_api/views.py
+++ src/project/drf_api/views.py
@@ -1,17 +1,17 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from slugify import slugify
-from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics, viewsets, permissions
 
 from project.blog_app.models import Post, Category
+from project.drf_api.permissions import IsAuthorOrReadOnly
 from project.drf_api.serializers import PostSerializer, CategorySerializer, FeedbackSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly]
     filter_backends = (
         DjangoFilterBackend,
         SearchFilter,
@@ -19,20 +19,34 @@ class PostViewSet(viewsets.ModelViewSet):
     )
     filterset_fields = ["category", "published"]
     search_fields = ["title", "content"]
-    ordering_fields = ["-created_at"]
+    ordering_fields = ["created_at"]
 
     def perform_create(self, serializer):
         title = serializer.validated_data["title"]
         slug = slugify(title)
         serializer.save(author=self.request.user, slug=slug)
 
-class CategoryListCreateAPIView(generics.ListCreateAPIView):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
+    )
+    filterset_fields = ["title"]
+    search_fields = ["title"]
+    ordering_fields = ["title"]
 
-class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [permissions.AllowAny()]
+        return [permissions.IsAdminUser()]
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data["title"]
+        slug = slugify(title)
+        serializer.save(slug=slug)
 
 class FeedbackCreateAPIView(generics.CreateAPIView):
     serializer_class = FeedbackSerializer
